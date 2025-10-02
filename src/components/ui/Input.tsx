fefd37bc80f272bd5d0b3c17ea3fe78ff6775f34
Gabriel Rojas <flexflit18@gmail.com>
2025-10-02 08:02:02 -0300

    refactor: set text color to black

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -6,7 +6,7 @@ const Input = React.forwardRef<HTMLInputElement,React.InputHTMLAttributes<HTMLIn
       <input
         type={type}
         className={cn(
-          "flex h-9 w-full rounded-md border border-gray-300 bg-white px-3 py-1 text-sm shadow-sm transition-colors",
+          "flex h-9 w-full rounded-md border border-gray-300 bg-white px-3 py-1 text-sm shadow-sm transition-colors text-black",
           
           "file:border-0 file:bg-transparent file:text-sm file:font-medium",
           
